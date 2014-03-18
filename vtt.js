2404af3cb7da628808f52ff841ec4b8a3b1da3f8
Rick Eyre <rick.eyre@hotmail.com>
2014-03-17 20:28:16 -0400

    Version 0.9.3.

--- vtt.js
+++ vtt.js
@@ -4221,8 +4221,6 @@
         // Enter BADWEBVTT state if header was not parsed correctly otherwise
         // another exception occurred so enter BADCUE state.
         self.state = self.state === "INITIAL" ? "BADWEBVTT" : "BADCUE";
-
-        throw e;
       }
       return this;
     },
